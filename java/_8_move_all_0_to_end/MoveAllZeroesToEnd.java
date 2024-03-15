3115ce02f61dfe7d5e1444cf8462e7f84d00ccec
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-03-15 23:45:35 +0530

    move all zeroes to end

---
+++ java/_8_move_all_0_to_end/MoveAllZeroesToEnd.java
@@ -0,0 +1,26 @@
+package _8_move_all_0_to_end;
+
+import common.Common;
+
+public class MoveAllZeroesToEnd {
+    public static int[] moveAllZeroes(int[] arr) {
+        int j = 0;
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] != 0 && arr[j] == 0) {
+                var temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+
+            if (arr[j] != 0) {
+                j++;
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Common.printArray(moveAllZeroes(new int[] { 8, 1, 0, 2, 1, 0, 3 }));
+    }
+}
