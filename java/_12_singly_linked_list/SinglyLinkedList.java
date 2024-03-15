1799e3459eaee18052d61af51d1ad3be359fbac3
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-03-16 00:16:01 +0530

    print elements of singly linked list

--- java/_12_singly_linked_list/SinglyLinkedList.java
+++ java/_12_singly_linked_list/SinglyLinkedList.java
@@ -14,16 +14,27 @@ public class SinglyLinkedList {
         }
     }
 
+    private void print() {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.data + "-->");
+            current = current.next;
+        }
+        System.out.print("null");
+    }
+
     public static void main(String[] args) {
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
         singlyLinkedList.head = new ListNode(10);
 
-        ListNode secondNode = new ListNode(20);
-        ListNode thirdNode = new ListNode(30);
-        ListNode fourthNode = new ListNode(40);
+        ListNode secondNode = new ListNode(1);
+        ListNode thirdNode = new ListNode(8);
+        ListNode fourthNode = new ListNode(11);
 
         singlyLinkedList.head.next = secondNode;
         secondNode.next = thirdNode;
         thirdNode.next = fourthNode;
+
+        singlyLinkedList.print();
     }
 }
