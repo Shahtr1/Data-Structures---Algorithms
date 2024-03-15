1bb78eaed24d094d3e1948a2ec89e9bf71d8175d
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-03-16 01:00:49 +0530

    insert nodes in linked list

--- java/_12_singly_linked_list/SinglyLinkedList.java
+++ java/_12_singly_linked_list/SinglyLinkedList.java
@@ -1,5 +1,8 @@
 package _12_singly_linked_list;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 public class SinglyLinkedList {
 
     private ListNode head;
@@ -20,7 +23,69 @@ public class SinglyLinkedList {
             System.out.print(current.data + "-->");
             current = current.next;
         }
-        System.out.print("null");
+        System.out.println("null");
+    }
+
+    private int length() {
+        int count = 0;
+        ListNode current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    private void insertAtBeginning(ListNode newNode) {
+        newNode.next = head;
+        head = newNode;
+    }
+
+    private void insertAtEnd(ListNode newNode) {
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        ListNode currentNode = head;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+
+        currentNode.next = newNode;
+
+    }
+
+    private Optional<ListNode> getNodeAtIndex(int index) {
+        int count = 0;
+        ListNode currentNode = head;
+        while (count != index) {
+            currentNode = currentNode.next;
+            count++;
+        }
+
+        return Optional.ofNullable(currentNode);
+    }
+
+    private void insertAtPosition(ListNode newNode, int position) {
+        if (position == 0) {
+            insertAtBeginning(newNode);
+        } else if (position == length()) {
+            insertAtEnd(newNode);
+        } else if (position > length() || position < 0) {
+            throw new IllegalArgumentException("Invalid position");
+        } else {
+            int count = 0;
+            ListNode currentNode = head;
+            while (count != position - 1) {
+                currentNode = currentNode.next;
+                count++;
+            }
+            ListNode temp = currentNode.next;
+            currentNode.next = newNode;
+            newNode.next = temp;
+        }
+
     }
 
     public static void main(String[] args) {
@@ -36,5 +101,31 @@ public class SinglyLinkedList {
         thirdNode.next = fourthNode;
 
         singlyLinkedList.print();
+        System.out.println("length is: " + singlyLinkedList.length());
+
+        ListNode zeroNode = new ListNode(90);
+        singlyLinkedList.insertAtBeginning(zeroNode);
+
+        singlyLinkedList.print();
+        System.out.println("length after insert is: " + singlyLinkedList.length());
+
+        ListNode endNode = new ListNode(150);
+        singlyLinkedList.insertAtEnd(endNode);
+
+        singlyLinkedList.print();
+        System.out.println("length after insert is: " + singlyLinkedList.length());
+
+        ListNode receivedNode = singlyLinkedList.getNodeAtIndex(0)
+                .orElseThrow(() -> new NoSuchElementException("No node found at the given index"));
+
+        System.out.println(receivedNode.data);
+
+        ListNode insertAtPosition4 = new ListNode(450);
+        singlyLinkedList.insertAtPosition(insertAtPosition4, 4);
+
+        singlyLinkedList.print();
+        System.out.println("length after insert is: " + singlyLinkedList.length());
+
     }
+
 }
