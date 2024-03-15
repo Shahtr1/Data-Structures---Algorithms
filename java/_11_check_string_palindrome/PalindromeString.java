57658a298097bf11c5af2363de825d361723ab77
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2024-03-16 00:01:46 +0530

    added isPalindrome

---
+++ java/_11_check_string_palindrome/PalindromeString.java
@@ -0,0 +1,24 @@
+package _11_check_string_palindrome;
+
+public class PalindromeString {
+
+    public static boolean isPalindrome(String word) {
+        char[] charArray = word.toCharArray();
+        int start = 0;
+        int end = charArray.length - 1;
+        while (start < end) {
+            if (charArray[start] != charArray[end]) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("madam"));
+    }
+}
