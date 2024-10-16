9ddeca809104276cd443421f73f6865aaba5febb
Shahtr1 <tramboos123@gmail.com>
2024-10-16 17:45:42 +0530

    How do you implement a Doubly Linked List? How do you print elements of a Doubly Linked List? How do you insert a node at the beginning of a Doubly Linked List? How do you insert a node at the end of a Doubly Linked List? How do you delete the first node in a Doubly Linked List? How do you delete the last node in a Doubly Linked List?

--- java/_5_doubly_linked_list/DoublyLinkedListTest.java
+++ java/_5_doubly_linked_list/DoublyLinkedListTest.java
@@ -1,7 +1,67 @@
 package _5_doubly_linked_list;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DoublyLinkedListTest {
+    private DoublyLinkedList doublyLinkedList;
 
+    @BeforeEach
+    void setUp() {
+        doublyLinkedList = new DoublyLinkedList();
+    }
+
+    @Test
+    void testAddNodeAtEnd_shouldAddNodesToTheList() {
+        addNodesToEndOfList(4);
+        verifyStringArrayInAscendingOrder(4);
+        assertEquals("1 -> 2 -> 3 -> 4", doublyLinkedList.printListForward());
+        assertEquals("4 <- 3 <- 2 <- 1", doublyLinkedList.printListBackward());
+    }
+
+    @Test
+    void testAddNodeAtBeginning_shouldAddNodesToTheList() {
+        addNodesToBeginningOfList(4);
+        assertEquals("1 <- 2 <- 3 <- 4", doublyLinkedList.printListBackward());
+        assertEquals("4 -> 3 -> 2 -> 1", doublyLinkedList.printListForward());
+    }
+
+    private boolean areTwoListsEqual(List<Integer> list1, List<Integer> list2) {
+        if (list1.size() != list2.size()) {
+            throw new IllegalArgumentException("Both lists should be of same size");
+        }
+        for (int i = 0; i < list1.size(); i++) {
+            if (list1.get(i) != list2.get(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void verifyStringArrayInAscendingOrder(int length) {
+        int[] dataArray = doublyLinkedList.getListOfInts().stream().mapToInt(i -> i).toArray();
+        IntStream.range(0, length).forEach(index -> {
+            assertEquals(index + 1, dataArray[index]);
+        });
+
+        assertEquals(length, dataArray.length);
+    }
+
+    private void addNodesToEndOfList(int numberOfNodes) {
+        IntStream.range(0, numberOfNodes).forEach(index -> {
+            doublyLinkedList.addNodeAtEnd(new ListNode(index + 1));
+        });
+    }
+
+    private void addNodesToBeginningOfList(int numberOfNodes) {
+        IntStream.range(0, numberOfNodes).forEach(index -> {
+            doublyLinkedList.addNodeAtBeginning(new ListNode(index + 1));
+        });
+    }
 }
