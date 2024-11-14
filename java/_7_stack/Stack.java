21b75477860a9aa0cbf12d4c98a1087c59f5c749
Shahtr1 <tramboos123@gmail.com>
2024-11-14 19:56:23 +0530

    Finished Stack

---
+++ java/_7_stack/Stack.java
@@ -0,0 +1,62 @@
+package _7_stack;
+
+import java.util.EmptyStackException;
+
+public class Stack {
+
+    private int size = 10;
+
+    private int[] array = new int[size];
+
+    private int top = -1;
+
+    public Stack() {
+    }
+
+    public Stack(int size) {
+        this.size = size;
+        array = new int[this.size];
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public boolean isStackFull() {
+        return top == getSize() - 1;
+    }
+
+    public void push(int item) {
+        if (isStackFull()) {
+            throw new StackOverflowError();
+        }
+        array[++top] = item;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return array[top];
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return array[top--];
+    }
+
+    private boolean isEmpty() {
+        return top == -1;
+    }
+
+    public int length() {
+        return top + 1;
+    }
+
+    public void clear() {
+        top = -1;
+    }
+
+}
